292d2d31cbadb6b16007a907f1c15aed6120e392
limintae@gmail.com <limintae@gmail.com>
2021-07-26 20:09:13 +0900

    불필요 소스 삭제

--- src/main/java/com/lim/example/jpa/web/rest/MemberController.java
+++ src/main/java/com/lim/example/jpa/web/rest/MemberController.java
@@ -46,6 +46,4 @@ public class MemberController {
         return true;
     }
 
-
-
 }
--- src/main/java/com/lim/example/jpa/web/rest/test/dao/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lim.example.jpa.web.rest.test.dao;
-
-import com.lim.example.jpa.web.rest.test.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
-
-
